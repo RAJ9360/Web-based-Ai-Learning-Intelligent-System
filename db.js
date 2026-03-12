79cb69421e9e60d462526d7cd9990e399182a3cb
RAJ9360 <rajkumarn20602@gmail.com>
2026-03-12 11:59:06 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2";
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "learning_ai"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL connection failed ❌", err);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
+});
+
+export default db;
